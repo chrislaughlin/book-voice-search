beb205856b80ec446d7bd0bd8c8aa0584981e301
Chris Laughlin <bluntm@gmail.com>
2021-05-10 18:09:29 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import { useState, useEffect } from "react";
+
+import "./styles.css";
+import Mic from "./microphone-black-shape.svg";
+
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const speech = new SpeechRecognition();
+speech.continuous = true;
+
+export default function App() {
+  const [isListening, setIsListening] = useState(false);
+  const [text, setText] = useState("");
+
+  const listen = () => {
+    setIsListening(!isListening);
+    if (isListening) {
+      speech.stop();
+    } else {
+      speech.start();
+    }
+  };
+
+  useEffect(() => {
+    speech.onresult = (event) => {
+      console.log(event);
+      setText(event.results[event.results.length - 1][0].transcript);
+    };
+    speech.start = () => {
+      console.log("start");
+    };
+    speech.onend = () => {
+      console.log("onend");
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="app">
+        <h2>Book Voice Search</h2>
+        <h3>Click the Mic and say an autors name</h3>
+        <div>
+          <img
+            className={`microphone ${isListening && "isListening"}`}
+            src={Mic}
+            alt="microphone"
+            onClick={listen}
+          />
+        </div>
+        <p>{text}</p>
+      </div>
+      <div className="icon-reg">
+        Icons made by{" "}
+        <a
+          href="https://www.flaticon.com/authors/dave-gandy"
+          title="Dave Gandy"
+        >
+          Dave Gandy
+        </a>{" "}
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </div>
+    </>
+  );
+}
